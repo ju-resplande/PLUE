28c0e0c927f8ea277345ae3b8c686f61b10187ff
Juliana Resplande <julianarsg13@gmail.com>
2020-06-05 15:04:21 -0300

    MNLI translation

--- code/batch.py
+++ code/batch.py
@@ -1,13 +1,20 @@
+!pip install transformers --upgrade
+!pip install mosestokenizer
+
+# Translate sentences to English and save mapping dicitionary as json.
+# Based on https://github.com/ruanchaves/assin/blob/master/sources/translate.py
+
 import json
 import math
 import os
+import torch
 
 from tqdm.notebook import tqdm
 from transformers import MarianMTModel, MarianTokenizer
 
 model_name = 'Helsinki-NLP/opus-mt-en-ROMANCE'
 tokenizer = MarianTokenizer.from_pretrained(model_name)
-model = MarianMTModel.from_pretrained(model_name)
+model = MarianMTModel.from_pretrained(model_name).to("cuda")
 
 def decode(text):
     decoded = tokenizer.decode(
@@ -25,10 +32,17 @@ def needs_translation(sample, translations):
 def translation(sample):
     src_txts = [f'>>pt_BR<< {text}' for text in sample]
 
-    translation_sample = tokenizer.prepare_translation_batch(src_txts)
-    translated = model.generate(**translation_sample)
+    #translation_sample = tokenizer.prepare_translation_batch(src_txts)
+    #translated = model.generate(translation_sample)
+
+    model_inputs = tokenizer.prepare_translation_batch(src_txts).to("cuda")
+    generated_ids = model.generate(
+                                    model_inputs.input_ids, 
+                                    attention_mask=model_inputs.attention_mask, 
+                                    num_beams=2
+                                    )
     
-    translated = [decode(text) for text in translated]
+    translated = [decode(text) for text in generated_ids]
 
     return translated
 
@@ -50,6 +64,32 @@ def translate2dict(sentences, dictpath, batch_size):
     
         new_translations = dict(zip(keys, values))
         translations.update(new_translations)
-
+        torch.cuda.empty_cache()
         with open(dictpath, 'w+') as f:
             json.dump(translations, f)
+
+from pprint import pprint
+import pandas as pd
+import os
+
+sentences = list()
+length = 0
+
+for f in splits:
+    table = pd.read_csv(f, sep = '\t', quoting=3, error_bad_lines=False)
+    print(f) 
+    #print(table.head())
+    #print('\n'*3)
+    
+    for idx in label_idx:
+        label = table[idx].copy()
+        print(label)
+        print('\n'*2)
+
+        sentences.extend(list(label))
+        length = length + label.size
+        
+
+
+assert length == len(sentences)
+sentences = set(sentences)
